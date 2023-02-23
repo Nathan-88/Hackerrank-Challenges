2a3f9edaded00d3f2378589cd73740e614895979
Onwuka Chukwuebuka Nathan <onwukaebuka88@gmail.com>
2023-02-23 07:23:18 +0100

    loops with python

---
+++ loopsD5.py
@@ -0,0 +1,15 @@
+"""
+Given an integer,n , print its first 10 multiples. Each multiple n x i ( where 1 <= i <= 10) should be printed on a new line in the form: n x i = result.
+"""
+import sys
+import re
+import random
+import os
+import math
+
+
+if __name__ == '__main__':
+    n = int(input("input a number: ").strip())
+for i in range(1, 11):
+    result = n * i
+    print("{} x {} = {}".format(n, i, result))
