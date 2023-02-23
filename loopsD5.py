d69e065904e950f0bcfafc19c5951ba4c4285f71
Onwuka Chukwuebuka Nathan <onwukaebuka88@gmail.com>
2023-02-23 08:43:44 +0100

    loop in python

--- loopsD5.py
+++ loopsD5.py
@@ -1,4 +1,5 @@
 """
+DAY 5
 Given an integer,n , print its first 10 multiples. Each multiple n x i ( where 1 <= i <= 10) should be printed on a new line in the form: n x i = result.
 """
 import sys
