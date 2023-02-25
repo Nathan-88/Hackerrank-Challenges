17e2449563c39a19c39206f65850206bae0b15e8
Onwuka Chukwuebuka Nathan <onwukaebuka88@gmail.com>
2023-02-25 05:15:34 +0100

    Dictionaries and Maps

---
+++ Dict.py
@@ -0,0 +1,33 @@
+"""
+Day 8
+Given n names and phone numbers, assemble a phone book that maps friends' names to their respective phone numbers. You will then be given an unknown number of names to query your phone book for. For each  queried, print the associated entry from your phone book on a new line in the form name=phoneNumber; if an entry for  is not found, print Not found instead.
+
+Note: Your phone book should be a Dictionary/Map/HashMap data structure.
+
+Input Format:
+The first line contains an integer, n, denoting the number of entries in the phone book.
+Each of the n subsequent lines describes an entry in the form of 2 space-separated values on a single line. The first value is a friend's name, and the second value is an 8-digit phone number.
+
+After the n lines of phone book entries, there are an unknown number of lines of queries. Each line (query) contains a name to look up, and you must continue reading lines until there is no more input.
+"""
+len_dict = int(input().strip())
+dict_ = {}
+
+# Loop through the length of dict and add key-value pairs to the dictionary
+for i in range(len_dict):
+    key_value_pairs = input().split()
+    key = key_value_pairs[0]
+    value = key_value_pairs[1]
+    dict_[key] = value
+#print(dict_)
+
+# there are an unknown number of lines of queries. Each line (query) contains a name to look up, and you must continue reading lines until there is no more input.
+while True:
+    try:
+        name = input().strip()
+        if name in dict_.keys():
+            print("{}={}".format(name, dict_.get(name)))
+        else:
+            print("Not found")
+    except EOFError:
+        break
